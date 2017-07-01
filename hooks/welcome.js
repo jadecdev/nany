44632df7db9f8b8f0bfa6d2a4b17bc4d129691bc
jadecdev <chabaro.j@sfeir-ext.com>
2017-07-01 18:59:50 +0200

    Welcome hook

--- hooks/welcome.js
+++ hooks/welcome.js
@@ -3,7 +3,8 @@ const utils = require('./_utils');
 
 exports.welcome = function welcome(app) {
     let now = new Date();
+    let offset = now.getTimezoneOffset(); // always gives 0...
 
-    app.ask(`Now is ${now}. Hello! ${now.getTimezoneOffset()}`,
+    app.ask(`Now is ${now}. Hello!}`,
         utils.NO_INPUTS);
 }
