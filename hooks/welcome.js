af1df33be1ad4e9e8b638660715829c96a60d74a
jadecdev <chabaro.j@sfeir-ext.com>
2017-07-11 22:46:10 +0200

    new changes

--- hooks/welcome.js
+++ hooks/welcome.js
@@ -4,11 +4,18 @@ const utils = require('./_utils');
 exports.welcome = function welcome(app) {
     const permission = app.SupportedPermissions.DEVICE_PRECISE_LOCATION;
     // app.askForPermission('To get your city', permission);
-    app.tell(`<speak>Once upon a time there was an old mother pig who had three little pigs and not enough food to feed them. <voice gender="male">So when they were old enough,</voice> she sent them out into the world to seek their fortunes.
+    app.tell(`<speak version="1.1" 
+    xmlns="http://www.w3.org/2001/10/synthesis"
+     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://www.w3.org/2001/10/synthesis
+    http://www.w3.org/TR/speech-synthesis11/synthesis.xsd"
+       xml:lang="en-US">
+       
+       Once upon a time there was an old mother pig who had three little pigs and not enough food to feed them. <voice gender="male">So when they were old enough,</voice> she sent them out into the world to seek their fortunes.
 
     <emphasis>The first little pig was very lazy</emphasis>. <p xml:lang="fr">He didn't want to work at all </p> and he built his house out of straw. <voice age="6">The second little pig worked a little bit harder </voice> but he was somewhat lazy too and he built his house out of sticks. Then, they sang and danced and played together the rest of the day.
 
-    <prosody volume="+6dB">The third little pig </prosody> worked hard all day and built his house with bricks. It was a sturdy house complete with a fine fireplace and chimney. It looked like it could withstand the strongest winds.
+    <s><prosody volume="+6dB">The third little pig </prosody></s> worked hard all day and built his house with bricks. It was a sturdy house complete with a fine fireplace and chimney. It looked like it could withstand the strongest winds.
 
         The next day, a wolf happened to pass by the lane where the three little pigs lived; and he saw the straw house, and he smelled the pig inside. He thought the pig would make a mighty fine meal and his mouth began to water.
 
