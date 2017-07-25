5587c087aedf83bd5fb7839877be438105b6246d
jadecdev <chabaro.j@sfeir-ext.com>
2017-07-25 19:41:50 +0200

    test

--- hooks/welcome.js
+++ hooks/welcome.js
@@ -4,21 +4,23 @@ const utils = require('./_utils');
 exports.welcome = function welcome(app) {
     const permission = app.SupportedPermissions.DEVICE_PRECISE_LOCATION;
     // app.askForPermission('To get your city', permission);
+
+
     app.tell(`<speak xml:lang="fr-FR">
   
        Once upon a time there was an old mother pig who had three little pigs and not enough food to feed them. 
        <voice gender="female" variant="2"> WORKING So when they were old enough,</voice> 
        she sent them out into the world to seek their fortunes.
-    <break/>
-    <emphasis>The first little pig was very lazy</emphasis>. <p xml:lang="fr">
+    <break/> WORKING
+    <emphasis> The first little pig was very lazy</emphasis>. <p xml:lang="fr">
     He didn't 
-    <voice gender="male" variant="1">want to work at all </voice></p> and he built <voice gender="female" variant="1">his house out of straw.</voice>
+    <voice gender="male" variant="1">WORKING want to work at all </voice></p> and he built <voice gender="male" variant="2">his house out of straw.</voice>
      <voice age="6">The second little pig worked a little bit harder </voice> 
      but he was somewhat lazy too and he built his house out of sticks. Then, they sang and danced and played together the rest of the day.
 
-    <s><prosody volume="+6dB">The third little pig </prosody></s> 
+    <prosody volume="+6dB">The third little pig </prosody>
     worked hard all day and built his house with bricks. 
-        <p><s>The next day, a wolf happened to pass by the lane where the three little pigs lived;</s>
+        <p><s> WORKING The next day, a wolf happened to pass by the lane where the three little pigs lived;</s>
          <s>and he saw the straw house,</s>
          <s>and he smelled the pig inside.</s>
           </p>He thought the pig would make a mighty fine meal and his mouth began to water. 
